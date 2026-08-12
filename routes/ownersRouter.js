94135c7ba6916612d0eabbf345420cebbcb4ead9
Dhruv Jain <dhruvjain0412@gmail.com>
2026-08-13 03:13:10 +0530

    Update .gitignore, configure MongoDB connection, and enhance ownersRouter with create functionality

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-models");
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owners = await ownerModel.find();
+
+    if (owners.length > 0) {
+      return res
+        .status(500)
+        .send("You Dont Have Permission To Create A New Owner");
+    }
+
+    let { fullname, email, password } = req.body;
+
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.status(201).send(createdOwner);
+  });
+}
 
 router.get("/", (req, res) => {
   res.send("Hey its owner");
