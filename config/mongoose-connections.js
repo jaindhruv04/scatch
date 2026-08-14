5a3044f59ce4f28001f20781c57714e6ded01860
Dhruv Jain <dhruvjain0412@gmail.com>
2026-08-15 00:28:58 +0530

    feat: update project name to Solar, adjust MongoDB connection, and remove unused images and files

--- config/mongoose-connections.js
+++ config/mongoose-connections.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 mongoose
-  .connect(`${config.get("MONGODB_URI")}/scatch`)
+  .connect(`${config.get("MONGODB_URI")}/solar`)
   .then(function () {
     console.log("connected");
   })
